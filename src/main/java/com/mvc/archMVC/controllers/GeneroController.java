54de8e2e6fef305f2f883bd760303337c94aa792
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-07-11 00:18:23 -0600

    [add methods in GeneroService & correction in GeneroController add to enpoint type Post /genero] & method type get /genero/{id}

--- src/main/java/com/mvc/archMVC/controllers/GeneroController.java
+++ src/main/java/com/mvc/archMVC/controllers/GeneroController.java
@@ -7,29 +7,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mvc.archMVC.dao.IGeneroRepository;
 import com.mvc.archMVC.entities.Genero;
+import com.mvc.archMVC.services.IGeneroService;
 
 @RestController
 public class GeneroController {
 	
 	@Autowired
-	public IGeneroRepository generoRepository;
+	public IGeneroService generoService;
 	
-	public GeneroController(IGeneroRepository generoRepository) {
-		this.generoRepository = generoRepository;
+	public GeneroController(IGeneroService generoService) {
+		this.generoService = generoService;
 	}
 	@PostMapping("/genero")
 	public int GuardarUnGenero(@RequestParam String  nombre) {
 		Genero genero = new Genero();
 		genero.setNombre(nombre);
-		generoRepository.save(genero);
+		generoService.save(genero);
 		return genero.getId();
 	}
 	
-	@GetMapping("genero/{nombre}/{id}")
+	@GetMapping("/genero/{id}")
 	public String buscarPorId(@PathVariable(name="id") int id) {
-		return generoRepository.findById(0).getNombre();
+		return generoService.findById(id).getNombre();
 	}
 
 }
--- src/main/java/com/mvc/archMVC/services/GeneroService.java
+++ src/main/java/com/mvc/archMVC/services/GeneroService.java
@@ -1,5 +1,37 @@
 package com.mvc.archMVC.services;
 
-public class GeneroService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mvc.archMVC.dao.IGeneroRepository;
+import com.mvc.archMVC.entities.Genero;
+
+@Service
+public class GeneroService implements IGeneroService{
+	
+	@Autowired
+	private IGeneroRepository generoServide;
+	
+	@Override
+	public void save(Genero genero) {
+		generoServide.save(genero);
+	}
+	
+	@Override
+	public void delete(int id) {
+		generoServide.deleteById(id);
+	}
+	
+	@Override
+	public Genero findById(int id) {
+		return generoServide.findById(id).orElse(null);
+	}
+	
+	@Override
+	public List<Genero> findAll(){
+		return (List<Genero>) generoServide.findAll();
+	}
 
 }
