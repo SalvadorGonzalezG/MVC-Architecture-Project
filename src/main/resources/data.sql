dfc81d035fb82ecfa8265e192136162824ad1b0f
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-07-21 23:59:39 -0600

    [ADD] - proof one actor in .sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,4 @@ INSERT INTO generos(nombre) VALUES ('Ciencia Ficción');
 
 INSERT INTO actor(nombre, url_imagen) VALUES('The Rock', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imdb.com%2Fname%2Fnm0425005%2Fbio%2F&psig=AOvVaw3JBLHJPxpr-WubyfgvqUG0&ust=1752643132266000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMD4rc2Ovo4DFQAAAAAdAAAAABAE');
 INSERT INTO actor(nombre, url_imagen) VALUES('Scarlett Johanson', 'https://hips.hearstapps.com/hmg-prod/images/scarlett-johanson-2-1658845768.jpg?crop=0.668xw:1.00xh;0.154xw,0&resize=640:*');
+INSERT INTO actor(nombre, url_imagen) VALUES('Girls Season', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Q5iFdd8DFg9JELhd_xTuKBDb_3NErNStFg&s');
